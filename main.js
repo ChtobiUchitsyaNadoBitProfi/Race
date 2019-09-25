b84f30a8242dc85754136591847371d98d351b59
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-09-25 22:19:31 +0700

    v4

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
+    easy = document.querySelector('.easy'),
+    medium = document.querySelector('.medium'),
+    hard = document.querySelector('.hard'),
     gameArea = document.querySelector('.gameArea'),
     car = document.createElement('div');
     car.classList.add('car');
 
+
+
+easy.addEventListener('click', easyMode);
+medium.addEventListener('click', mediumMode);
+hard.addEventListener('click', hardMode);
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
 document.addEventListener('keyup', stopRun);
@@ -18,9 +26,33 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 5,
-  traffic: 7
+  speed: 3,
+  traffic: 2
 };
+
+function easyMode(){
+  setting.speed = 3;
+  setting.traffic = 8;
+  easy.style.background = "purple";
+  medium.style.background = null;
+  hard.style.background = null;
+}
+
+function mediumMode(){
+  setting.speed = 4;
+  setting.traffic = 5;
+  medium.style.background = "purple";
+  easy.style.background = null;
+  hard.style.background = null;
+}
+
+function hardMode(){
+  setting.speed = 5;
+  setting.traffic = 2;
+  hard.style.background = "purple";
+  medium.style.background = null;
+  easy.style.background = null;
+}
 
 function getQuantityElementElements(heightElement){
   return document.documentElement.clientHeight / heightElement + 1;
@@ -30,7 +62,8 @@ function startGame() {
   let x;
   start.classList.add('hide');
   gameArea.classList.remove('hide');
-
+  gameArea.innerHTML = '';
+  
   for (let i = 0; i < getQuantityElementElements(100); i++){
     const line = document.createElement('div');
     line.classList.add('line');
@@ -73,7 +106,7 @@ function startGame() {
         gameArea.appendChild(enemy);
         break;
       case 7:  // if (x === 'value1')
-        enemy.style.background = 'transparent url("police.png") center / cover no-repeat';
+        enemy.style.background = 'transparent url("police1.png") center / cover no-repeat';
         gameArea.appendChild(enemy);
         break;
       case 8:  // if (x === 'value2')
@@ -86,8 +119,11 @@ function startGame() {
         break;
     } 
   }
-
+  setting.score = 0;
   gameArea.appendChild(car);
+  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth/2;
+  car.style.top = 'auto';
+  car.style.bottom = '10px';
   setting.start = true;
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
@@ -96,6 +132,8 @@ function startGame() {
 
 function playGame() {
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + Math.floor(setting.score);
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0){
@@ -111,9 +149,15 @@ function playGame() {
     }
     if(keys.ArrowUp && setting.y > 0) {
       setting.y -= setting.speed;
+      if(setting.speed <= 7){
+        setting.speed += 0.05;
+      }
     }
     if(keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)){
       setting.y += setting.speed;
+      if(setting.speed > 1.5){
+        setting.speed -= 0.05;
+      }      
     }
     
     car.style.left = setting.x + 'px';
@@ -125,6 +169,19 @@ function playGame() {
 function moveEnemy(){
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item){
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom && carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right && carRect.bottom >= enemyRect.top){
+        setting.start = false;
+        setting.speed = 2;
+        start.classList.remove('hide');
+        // gameArea.classList.add('hide');
+
+        score.style.top = start.offsetHeight;
+    }
+
     item.y += (setting.speed / 2) + 1;
     item.style.top = item.y + 'px';
     if(item.y >= document.documentElement.clientHeight){
@@ -133,7 +190,6 @@ function moveEnemy(){
     }
   });
 }
-
 
 function startRun(event) {
   event.preventDefault();
