bd597f8ab73dbd3abac9cab46890f3dc8a3fc328
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-09-25 15:05:27 +0700

    v3

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 4,
-  traffic: 3
+  speed: 5,
+  traffic: 7
 };
 
 function getQuantityElementElements(heightElement){
@@ -27,7 +27,9 @@ function getQuantityElementElements(heightElement){
 }
 
 function startGame() {
+  let x;
   start.classList.add('hide');
+  gameArea.classList.remove('hide');
 
   for (let i = 0; i < getQuantityElementElements(100); i++){
     const line = document.createElement('div');
@@ -36,16 +38,53 @@ function startGame() {
     line.y = i * 100;
     gameArea.appendChild(line);
   }
-
-  for (let i = 0; i < getQuantityElementElements(100 * setting.traffic); i++){
+  
+  for (let i = 0; i < getQuantityElementElements(80 * setting.traffic); i++){
     const enemy = document.createElement('div');
     enemy.classList.add('enemy');
-    enemy.y = -100 * setting.traffic * (i + 1);
+    enemy.y += Math.random() * 2;
+    enemy.y = -50 * setting.traffic * (i + 1);
     enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - car.clientWidth)) + 'px';
     enemy.style.top = enemy.y + 'px';
-    enemy.style.background = 'transparent url("1600.png") center / cover no-repeat';
-    gameArea.appendChild(enemy);
-
+    x = Math.floor(Math.random() * Math.floor(10));
+    switch(x) {
+      case 1:  // if (x === 'value1')
+        enemy.style.background = 'transparent url("red1.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+      case 2:  // if (x === 'value2')
+        enemy.style.background = 'transparent url("blue1.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+      case 3:  // if (x === 'value1')
+        enemy.style.background = 'transparent url("green1.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+      case 4:  // if (x === 'value2')
+        enemy.style.background = 'transparent url("grey4.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+      case 5:  // if (x === 'value1')
+        enemy.style.background = 'transparent url("purple3.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+      case 6:  // if (x === 'value2')
+        enemy.style.background = 'transparent url("blue4.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+      case 7:  // if (x === 'value1')
+        enemy.style.background = 'transparent url("police.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+      case 8:  // if (x === 'value2')
+        enemy.style.background = 'transparent url("yellow3.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;  
+      default:
+        enemy.style.background = 'transparent url("red2.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
+        break;
+    } 
   }
 
   gameArea.appendChild(car);
@@ -61,9 +100,14 @@ function playGame() {
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0){
       setting.x -= setting.speed;
+      car.style.transform = 'rotate(-10deg)';
+    }
+    if (keys.ArrowLeft === false){
+      car.style.transform = 'rotate(0deg)';
     }
     if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)){
       setting.x += setting.speed;
+      car.style.transform = 'rotate(10deg)';
     }
     if(keys.ArrowUp && setting.y > 0) {
       setting.y -= setting.speed;
@@ -81,13 +125,12 @@ function playGame() {
 function moveEnemy(){
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item){
-    item.y += setting.speed / 2;
+    item.y += (setting.speed / 2) + 1;
     item.style.top = item.y + 'px';
     if(item.y >= document.documentElement.clientHeight){
       item.y = -100 * setting.traffic;
       item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - car.clientWidth)) + 'px';
     }
-
   });
 }
 
